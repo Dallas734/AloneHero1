93c9dc402ac9b7f2aff3b6777b485d51203df7d5
Mark <vernidub66@gmail.com>
2022-10-10 21:54:01 +0300

    Jump animation done!

--- AloneHero1/Player.cpp
+++ AloneHero1/Player.cpp
@@ -3,15 +3,15 @@
 
 States Player::Jump(bool& keyPressed, float time, double xBeginSprite, double yBeginSprite, double width, double height, int frames, Directions direction, RenderWindow& window)
 {
+	this->state = JUMP;
 	SetSprite("Jump.png", JUMP, xBeginSprite, yBeginSprite, width, height);
 	currentFrame += time * 0.01;
 	if (this->currentFrame > frames) this->currentFrame -= frames;
 
 	if (onGround)
 	{
-		dy = -10;
+		dy = -0.1; // Высота прыжка
 		onGround = false;
-		this->state = JUMP;
 	}
 
 	if (direction == RIGHT)
@@ -28,22 +28,17 @@ States Player::Jump(bool& keyPressed, float time, double xBeginSprite, double yB
 		spriteJump.setOrigin({ spriteJump.getLocalBounds().width, 0 });
 		spriteJump.setScale(-1, 1);
 	}
-	x += dx * time;
+
 	y += dy * time;
 	dx = 0;
-	dy = 0;
-	spriteJump.setTextureRect(IntRect(width /** int(currentFrame)*/, yBeginSprite, width, height));
+
+	spriteJump.setTextureRect(IntRect(width * int(currentFrame), yBeginSprite, width, height));
 	spriteJump.setPosition(x, y);
 	window.clear();
 	window.draw(GetSprite(JUMP));
 	window.display();
 	
 	return JUMP;
-}
-
-void Player::Control()
-{
-
 }
 
 void Player::HelthUP(int regenerationUnits)
@@ -63,14 +58,11 @@ void Player::Update(float time, RenderWindow& window)
 
 	// Уровень земли и падение
 	int ground = 300;
-	if (!onGround || state == JUMP)
+	if (!onGround)
 	{
 		dy += 0.0001 * time;
-		y += dy * time;
-		x += dx * time;
-		// onGround = true;
 	}
-	//onGround = false;
+	y += dy * time;
 	if (y > ground)
 	{
 		y = ground;
@@ -108,81 +100,12 @@ void Player::Update(float time, RenderWindow& window)
 		state = IDLE;
 		Idle(time, 0, 0, this->width, this->height, 8, direction, window);
 	}
-
-	if (Keyboard::isKeyPressed(Keyboard::Space) && onGround)
+	
+	if ((Keyboard::isKeyPressed(Keyboard::Space) && onGround) || (!onGround && state == JUMP))
 	{
 		state = JUMP;
 		Jump(keyPressed, time, 0, 0, this->width, this->height, 2, direction, window);
 	}
 
-	/*switch (state)
-	{
-	case RUN:
-		SetSprite("Run.png", RUN, 0, 0, width, height);
-		currentFrame += time * 0.01;
-		if (this->currentFrame > 8) this->currentFrame -= 8;
-		switch (direction)
-		{
-		case RIGHT:
-			dx = speed;
-			spriteMove.setOrigin({ 0, 0 });
-			spriteMove.setScale(1, 1);
-			break;
-		case LEFT:
-			dx = -speed;
-			spriteMove.setOrigin({ spriteMove.getLocalBounds().width, 0 });
-			spriteMove.setScale(-1, 1);
-			break;
-		}
-		spriteMove.setTextureRect(IntRect(width * int(currentFrame), 0, width, height));
-		window.clear();
-		spriteMove.setPosition(x, y);
-		window.draw(GetSprite(RUN));
-		window.display();
-		break;
-	case HIT:
-		SetSprite("Hit1.png", HIT, 0, 0, width, height);
-		currentFrame += time * 0.01;
-		if (this->currentFrame > 4) this->currentFrame -= 4;
-		switch (direction)
-		{
-		case RIGHT:
-			spriteHit.setOrigin({ 0, 0 });
-			spriteHit.setScale(1, 1);
-			break;
-		case LEFT:
-			spriteHit.setOrigin({ spriteHit.getLocalBounds().width, 0 });
-			spriteHit.setScale(-1, 1);
-			break;
-		}
-		spriteHit.setTextureRect(IntRect(width * int(currentFrame), 0, width, height));
-		spriteHit.setPosition(x, y);
-		window.clear();
-		window.draw(GetSprite(HIT));
-		window.display();
-		break;
-	case IDLE:
-		SetSprite("Idle.png", IDLE, 0, 0, width, height);
-		currentFrame += time * 0.005;
-		if (this->currentFrame > 8) this->currentFrame -= 8;
-		switch (direction)
-		{
-		case RIGHT:
-			spriteIdle.setOrigin({ 0, 0 });
-			spriteIdle.setScale(1, 1);
-			break;
-		case LEFT:
-			spriteIdle.setOrigin({ spriteIdle.getLocalBounds().width, 0 });
-			spriteIdle.setScale(-1, 1);
-		}
-		spriteIdle.setTextureRect(IntRect(width* int(currentFrame), yBeginSprite, width, height));
-		spriteIdle.setPosition(x, y);
-		window.clear();
-		window.draw(GetSprite(IDLE));
-		window.display();
-		break;
-	case JUMP:
-
-	}*/
 }
 
--- AloneHero1/Player.h
+++ AloneHero1/Player.h
@@ -12,14 +12,10 @@ class Player : public Entity
 		};
 		~Player() {};
 		States Jump(bool& keyPressed, float time, double xBeginSprite, double yBeginSprite, double width, double height, int frames, Directions direction, RenderWindow& window);
-		void Control();
 		void HelthUP(int regenerationUnits);
 		void StrengthUP();
 		void SpeedUp(double improveUnits);
 		void Update(float time, RenderWindow& window) override;
-		/*States Move(float time, double width, double height, int frames, Directions direction) override;
-		int Hit(float time, double width, double height, int frames, double strength, Directions direction) override;
-		void Damage(float time, double width, double height, int frames, double strength, Directions direction) override;
-		States Idle(float time, double width, double height, int frames, Directions direction) override;*/
+		
 };
 
